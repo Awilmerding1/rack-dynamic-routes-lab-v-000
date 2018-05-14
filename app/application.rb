972da002153219c7056ec8408ed4aa9ba00c63ae
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-14 18:46:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
     item = Item.find{|i| i.name == item_name}
-    if item_name != ""
+    if item
       resp.write item_name.price
     else
       resp.write "Route not found"
